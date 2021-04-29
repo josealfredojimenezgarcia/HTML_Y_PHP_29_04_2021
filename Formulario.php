22a79cf0ee38d3b53b44a4da1fc7ae5694dd46f4
josealfredojimenezgarcia <joseajimenez07@gmail.com>
2021-04-29 17:04:59 -0500

    Crear Formulario

---
+++ Formulario.php
@@ -0,0 +1,23 @@
+<?php
+
+
+    echo <<<NOMBRE
+    Tu nombre es: $_REQUEST[nombre] <br>
+NOMBRE;
+    echo <<<EDAD
+    Tu edad es: $_REQUEST[edad] <br>
+EDAD;
+    echo <<<GENERO
+    Tu genero es: $_REQUEST[Genero] <br>
+GENERO;
+    echo <<<ESTADO
+    Tu Estado civil es: $_REQUEST[Estado_Civil] <br>
+ESTADO;
+    echo <<<ESTATURA
+    Tu Estatura es: $_REQUEST[Estatura] <br>
+ESTATURA;
+    echo <<<PESO
+    Tu Peso Corporal es: $_REQUEST[Peso_Corporal] <br>
+PESO;
+
+?>
